25d2754bc646528a5c0284944f71ae0a753660c8
Pushtikansara <pushti612@gmail.com>
2025-07-10 11:09:15 +0530

    final project

---
+++ basics/js_1.js
@@ -0,0 +1,8 @@
+
+    function myFunction() {
+        var str = "Hello World!!";
+        //alert(str);blank page
+        document.write("<b>" + str + "<br/></b>");
+        document.write('First Javascript Program');
+        console.log(str);
+    }
